2626839a059f6697d004ce8e459b5fac3334e4d6
CJ <cjpais@ucsd.edu>
2015-06-16 20:20:11 -0700

    cleaned the code

--- Main.py
+++ Main.py
@@ -1,20 +1,30 @@
-"""
-Created on Thurs May 21 22:23:22 2015
-
-@author: Christopher Pais
-"""
-
 import sys
 import xml.etree.ElementTree as ET
 import helper as help
 import time
-import parse
+import parse as p
 
 #get filename from commandline
 file = sys.argv[1]
+atoms = []
+atom = []
+bonds = []
+bond = []
+angle = []
 
-#begin parsing, gets single atom.
+#begin parsing, gets single atom. do in method
 tree = ET.parse(file)
 root = tree.getroot()
 atomList = root.findall('./atomArray/atom')
 bondList = root.findall('./bondArray/bond')
+
+atom = p.create_atomobj(atomList)
+bond = p.create_bondobj(bondList)
+
+p.print_atoms(atom)
+p.print_bonds(bond)
+
+AngleList = p.print_find_angles(atom,bond)
+
+dihedrals = p.find_dihedrals(AngleList)
+p.print_dihedrals(dihedrals)
