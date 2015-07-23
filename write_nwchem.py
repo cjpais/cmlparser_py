d6ddb50629f38406d63ae4ced8571a64db988ee5
sipjca <cjpais@ucsd.edu>
2015-07-23 12:12:51 -0700

    fixed type causing bug

--- write_nwchem.py
+++ write_nwchem.py
@@ -8,7 +8,7 @@ def dft(dihedrals):
     counter = "A"
     for i in range(len(dihedrals)):
         if dihedrals[i].dft:
-            print 'geometry mol%s units angstroms noautoz noautosim' % counter
+            print 'geometry mol%s units angstroms noautoz noautosym' % counter
             print "%s %s %s %s" % (dihedrals[i].dihedral_master1.atom_type,dihedrals[i].dihedral_master1.x_pos,dihedrals[i].dihedral_master1.y_pos,dihedrals[i].dihedral_master1.z_pos)
             print "%s %s %s %s" % (dihedrals[i].dihedral_master2.atom_type,dihedrals[i].dihedral_master2.x_pos,dihedrals[i].dihedral_master2.y_pos,dihedrals[i].dihedral_master2.z_pos)
             print "%s %s %s %s" % (dihedrals[i].dihedral_slave1.atom_type,dihedrals[i].dihedral_slave1.x_pos,dihedrals[i].dihedral_slave1.y_pos,dihedrals[i].dihedral_slave1.z_pos)
